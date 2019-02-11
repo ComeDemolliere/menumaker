aecfb66d9774c33c60b523c9d2b9da499318c0aa
ComeDemolliere <come.demolliere@etu.unice.fr>
2019-02-11 16:30:12 +0100

    add an abstract class for view

---
+++ src/sample/view/View.java
@@ -0,0 +1,11 @@
+package sample.view;
+
+abstract class View {
+    public static String XML_FILE;
+
+    public static  String LABEL;
+
+    public static  int WIDTH;
+
+    public static  int HEIGHT;
+}
