b7a50619d8834d5187a59817aefd1035f3079d5a
RobinLambert1 <robin.lambert@etu.unice.fr>
2019-02-25 17:23:40 +0100

    Ajout d'un id pour les boutons de AccountSettings.fxml

--- src/sample/controller/AccountController.java
+++ src/sample/controller/AccountController.java
@@ -1,9 +1,19 @@
 package sample.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import sample.Page;
+
 public class AccountController extends Controller {
+
+    @FXML
+    private Button addProfile;
 
     @Override
     public void init(){
         super.init();
+        //addProfile.setOnAction(actionEvent -> this.router.change(Page.ADDGUEST));
     }
+
+
 }
