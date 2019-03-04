40b47f2039223fecf4225beae026383a97872237
RobinLambert1 <robin.lambert@etu.unice.fr>
2019-03-04 15:48:17 +0100

    New Profile model

--- src/models/Profile.java
+++ src/models/Profile.java
@@ -1,8 +1,30 @@
 package models;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Profile {
     private String name;
     private List<Ingredient> ingredientsNotGood;
+
+    public Profile(String name) {
+        this.name = name;
+        ingredientsNotGood = new ArrayList<>();
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public List<Ingredient> getIngredientsNotGood(){
+        return this.ingredientsNotGood;
+    }
+
+    public void addIngredient(Ingredient ingredient){
+        ingredientsNotGood.add(ingredient);
+    }
+    public void removeIngredient(Ingredient ingredient){
+        this.ingredientsNotGood.remove(ingredient);
+    }
+
 }
